6010d107ba7675b7095c9a172390cd71d644dd1e
Vachana Shetty <vachanagshetty@gmail.com>
2026-06-08 03:54:13 -0700

    Add: sum-of-two-integers - submission-6

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        
+        # res = a ^ b
+        # carry = (a & b) << 1
+
+        # while carry:
+        #     temp = res
+        #     res = res ^ carry
+        #     carry = (temp & carry) << 1
+        
+        # return res
+# These two wont handle infite loop wit negative carry->In Python, 
+# integers have infinite precision — unlike other languages where integers are 
+# fixed 32 bits,Python will keep extending the carry left forever.
+        # while b:
+        #     carry = (a & b) << 1
+        #     a = a ^ b
+        #     b = carry
+        # return a
+
+        mask = 0xFFFFFFFF
+        while b:
+            carry = ((a & b) << 1) & mask
+            a = (a ^ b) & mask
+            b = carry
+
+        return a if a <= 0x7FFFFFFF else ~(a^mask)
+
+    # SC and TC: O(1)
