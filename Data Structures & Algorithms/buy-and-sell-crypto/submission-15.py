74ecf6f7fd3e33a01bfedf455ceb1fe1712bda7d
Vachana Shetty <vachanagshetty@gmail.com>
2026-07-30 17:14:38 -0700

    Add: buy-and-sell-crypto - submission-15

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-15.py
@@ -0,0 +1,47 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        l = 0
+        res = 0
+
+        for r in range(1,len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+            else:
+                res = max(res, prices[r] - prices[l])
+
+        return res
+
+# O(n) and O(1)
+
+# They ask you to return the best days to buy and sell stock
+# class Solution:
+#     def maxProfitDays(self, prices):
+
+#         l = 0
+#         max_profit = 0
+
+#         buy_day = 0
+#         sell_day = 0
+
+#         for r in range(1, len(prices)):
+
+#             # found a cheaper buying price
+#             if prices[r] < prices[l]:
+#                 l = r
+
+#             else:
+#                 profit = prices[r] - prices[l]
+
+#                 if profit > max_profit:
+#                     max_profit = profit
+#                     buy_day = l
+#                     sell_day = r
+
+#         return [buy_day, sell_day]
+            
+
+
+
+             
+        
